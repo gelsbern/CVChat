745ff812fb5f340a165cd720ab3b0aad682ec5b5
BuildTools <frediw@users.noreply.github.com>
2017-04-07 15:30:07 +0200

    Added server part

---
+++ mc/src/main/java/org/cubeville/cvchat/CVChat.java
@@ -0,0 +1,124 @@
+package org.cubeville.cvchat;
+
+import java.util.Collection;
+import java.util.StringTokenizer;
+import java.util.UUID;
+
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import org.cubeville.cvipc.CVIPC;
+import org.cubeville.cvipc.IPCInterface;
+
+public class CVChat extends JavaPlugin implements Listener, IPCInterface
+{
+    CVIPC ipc;
+    
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
+
+        ipc = (CVIPC) pm.getPlugin("CVIPC");
+        ipc.registerInterface("locchat", this);
+        ipc.registerInterface("chatquery", this);
+    }
+
+    public void onDisable() {
+        ipc.deregisterInterface("locchat");
+        ipc.deregisterInterface("chatquery");
+    }
+    
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(null);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        event.setQuitMessage(null);
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(command.getName().equals("modreq")) {
+            if(!(sender instanceof Player)) return false;
+            Player player = (Player) sender;
+            
+            if(args.length == 0) {
+                sender.sendMessage("/modreq <message>");
+                return true;
+            }
+            
+            String text = "";
+            for(int i = 0; i < args.length; i++) {
+                if(i > 0) text += " ";
+                text += args[i];
+            }
+
+            Location loc = player.getLocation();
+            ipc.sendMessage("modreq", player.getUniqueId() + "|" + loc.getWorld().getName() + "," + loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ() + "|" + text);
+        }
+        return false;
+    }
+
+    public void process(String channel, String message) {
+        if(channel.equals("chatquery")) {
+            StringTokenizer tk = new StringTokenizer(message, "|");
+            if(tk.countTokens() != 4) return;
+            String channelName = tk.nextToken();
+            String mId = tk.nextToken();
+            UUID playerId = UUID.fromString(tk.nextToken());
+            String parameter = tk.nextToken();
+            if(!parameter.equals("health")) return;
+
+            Player player = getServer().getPlayer(playerId);
+            if(player == null) return;
+
+            double health = 20.0;
+            if(player.getGameMode() == GameMode.SURVIVAL) health = player.getHealth();
+            ipc.sendMessage("chatquery|" + channelName + "|" + mId + "|" + playerId.toString() + "|health=" + health);
+        }
+        else {
+            int idx = message.indexOf("|");
+            if(idx == -1) return;
+            
+            UUID playerId = UUID.fromString(message.substring(0, idx));
+            message = message.substring(idx + 1);
+            String greyMessage = "§7" + removeColorCodes(message);
+            
+            Player player = getServer().getPlayer(playerId);
+            if(player == null) return;
+            Location loc = player.getLocation();
+            
+            Collection<Player> players = (Collection<Player>) getServer().getOnlinePlayers();
+            for(Player p: players) {
+                Location pl = p.getLocation();
+                if(pl.getWorld().getUID().equals(loc.getWorld().getUID()) && pl.distance(loc) < 55) {
+                    p.sendMessage(message);
+                }
+                else {
+                    if(p.hasPermission("cvchat.monitor.local")) {
+                        p.sendMessage(greyMessage);
+                    }
+                }
+            }
+        }
+    }
+
+    private static String[] colorCodes = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "k", "l", "m", "n", "o", "r"};
+    public static String removeColorCodes(String text) {
+        String ret = text;
+        for(int i = 0; i < colorCodes.length; i++) {
+            ret = ret.replace("§" + colorCodes[i], "");
+        }
+        return ret;
+    }
+}
